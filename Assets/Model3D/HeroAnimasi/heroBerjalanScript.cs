6fb5041556f89855021174722cda1054e666942e
FikriSatria11 <muhfikrisatria@gmail.com>
2020-06-10 17:07:05 +0700

    first commit

---
+++ Assets/Model3D/HeroAnimasi/heroBerjalanScript.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class heroBerjalanScript : MonoBehaviour
+{
+   Animator anim;
+	Animator Animation;
+	public int Speed;
+	public int Jumping;
+	Rigidbody2D Jump;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+		Animation = GetComponent<Animator> ();
+		Jump = GetComponent<Rigidbody2D> ();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   
+
+        if (Input.GetKey (KeyCode.J)) { // Fungsi untuk mendapatkan kode Keyboard A.
+			transform.Translate (Vector2.left * Speed * Time.deltaTime); //Fungsi untuk gerakan karakter ke kiri sesuai dengan kecepatan input.
+		} else if (Input.GetKey (KeyCode.J) && Input.GetKey (KeyCode.Space)) { // Fungsi jika kode Keyboard yang di gunakan A dan Spasi maka tetap menerapkan fungsi gerak ke kiri.
+			transform.Translate (Vector2.left * Speed * Time.deltaTime);//Fungsi untuk gerakan karakter ke kiri sesuai dengan kecepatan input.
+			Animation.SetBool ("Walk Pocong", true); // Fungsi untuk menyalakan animasi berjalan.
+		} else { // Fungsi jika kondisi yang di hadapi selain Keyboard A dan Keyboard A+Spasi.
+			Animation.SetBool ("Walk Pocong", false); // Fungsi untuk mematikan animasi berjalan.
+		}
+		
+		if (Input.GetKey (KeyCode.L)) { // Fungsi untuk mendapatkan kode Keyboard D.
+			transform.Translate (Vector2.right * Speed * Time.deltaTime); //Fungsi untuk gerakan karakter ke kanan sesuai dengan kecepatan input.
+			Animation.SetBool ("Walk Pocong", true); // Fungsi untuk menyalakan animasi berjalan.
+		} else if (Input.GetKey (KeyCode.L) && Input.GetKey (KeyCode.Space)) { // Fungsi jika kode Keyboard yang di gunakan D dan Spasi maka tetap menerapkan fungsi gerak ke kanan.
+			transform.Translate (Vector2.right * Speed * Time.deltaTime); //Fungsi untuk gerakan karakter ke kanan sesuai dengan kecepatan input.
+			Animation.SetBool ("Walk Pocong", true); // Fungsi untuk menyalakan animasi berjalan.
+		} else { // Fungsi jika kondisi yang di hadapi selain Keyboard D dan Keyboard D+Spasi.
+			Animation.SetBool ("Walk Pocong", false); // Fungsi untuk mematikan animasi berjalan.
+		}
+		
+		if (Input.GetKey (KeyCode.I)) { // Fungsi untuk mendapatkan kode Keyboard D.
+			transform.Translate (Vector2.up * Speed * Time.deltaTime); //Fungsi untuk gerakan karakter ke kanan sesuai dengan kecepatan input.
+			Animation.SetBool ("Walk Pocong", true); // Fungsi untuk menyalakan animasi berjalan.
+		} else if (Input.GetKey (KeyCode.I) && Input.GetKey (KeyCode.Space)) { // Fungsi jika kode Keyboard yang di gunakan D dan Spasi maka tetap menerapkan fungsi gerak ke kanan.
+			transform.Translate (Vector2.up * Speed * Time.deltaTime); //Fungsi untuk gerakan karakter ke kanan sesuai dengan kecepatan input.
+			Animation.SetBool ("Walk Pocong", true); // Fungsi untuk menyalakan animasi berjalan.
+		} else { // Fungsi jika kondisi yang di hadapi selain Keyboard D dan Keyboard D+Spasi.
+			Animation.SetBool ("Walk Pocong", false); // Fungsi untuk mematikan animasi berjalan.
+		}
+		
+		if (Input.GetKey (KeyCode.K)) { // Fungsi untuk mendapatkan kode Keyboard D.
+			transform.Translate (Vector2.down * Speed * Time.deltaTime); //Fungsi untuk gerakan karakter ke kanan sesuai dengan kecepatan input.
+			Animation.SetBool ("Walk Pocong", true); // Fungsi untuk menyalakan animasi berjalan.
+		} else if (Input.GetKey (KeyCode.K) && Input.GetKey (KeyCode.Space)) { // Fungsi jika kode Keyboard yang di gunakan D dan Spasi maka tetap menerapkan fungsi gerak ke kanan.
+			transform.Translate (Vector2.down * Speed * Time.deltaTime); //Fungsi untuk gerakan karakter ke kanan sesuai dengan kecepatan input.
+			Animation.SetBool ("Walk Pocong", true); // Fungsi untuk menyalakan animasi berjalan.
+		} else { // Fungsi jika kondisi yang di hadapi selain Keyboard D dan Keyboard D+Spasi.
+			Animation.SetBool ("Walk Pocong", false); // Fungsi untuk mematikan animasi berjalan.
+		}
+		
+		
+		if (Input.GetKey (KeyCode.Space)) { // Fungsi untuk mendapatkan kode Keyboard Space.
+			Jump.AddForce (new Vector2 (1, Jumping)); //Fungsi untuk melompat menggunakan Vector 2 dengan kecepatan input Jumping.
+			Animation.SetBool ("Jump Pocong", true); // Fungsi untuk menyalakan animasi melompat.
+		}  else { // Fungsi jika kondisi yang di hadapi selain Keyboard Space.
+			Animation.SetBool ("Jump Pocong", false); // Fungsi untuk mematikan animasi melompat.
+		}
+
+        if(Input.GetKeyDown(KeyCode.O)){
+            anim.SetTrigger("Animasi Jalan");
+        }
+                               
+        if(Input.GetKeyDown(KeyCode.P)){
+            anim.SetTrigger("Animasi Lari");
+        }
+    }
+}
---
+++ Assets/playerMovement.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+
+    public Animator animator;
+
+    public CharacterController controller;
+
+    public float speed = 12f;
+    public float gravity = -9.81f;
+    public float jumpHeight = 3f;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    Vector3 velocity;
+    bool isGrouded;
+
+    void start(){
+
+        animator = GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        isGrouded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        animator.SetBool("Mendarat", isGrouded);
+        
+
+        if(isGrouded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+        bool isMove = CheckMove(move);
+
+        animator.SetBool("lari" , isMove);
+
+        controller.Move(move * speed * Time.deltaTime);
+
+        if(Input.GetButtonDown("Jump") && isGrouded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+            animator.SetBool("lompat", true);
+        }else if(isGrouded){
+            animator.SetBool("lompat", false);
+
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
+    }
+
+    bool CheckMove (Vector3 move){
+        if(move.x != 0 || move.z != 0){
+            return true;
+        }
+        return false;
+    }
+}
